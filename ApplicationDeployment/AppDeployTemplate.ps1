1b4ce8b879a7076ad36d732bc7cad78df53db440
PaulTizzone <paul.tizzone@outlook.com>
2026-05-28 17:53:49 +1000

    New WIP.

--- ApplicationDeployment/AppDeployTemplate.ps1
+++ ApplicationDeployment/AppDeployTemplate.ps1
@@ -1,1 +1,33 @@
-# Restarting...
+# Script Variables
+$MirrorAuthUser = $null;
+$MirrorAuthPass = $null;
+$ProgressPreference = 'SilentlyContinue';
+
+# Set up common use functions
+function Get-FileFromURL {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$fileURL,
+        [Parameter(Mandatory=$true)]
+        [String]$outFile,
+        [String]$fileHash
+    )
+    Invoke-WebRequest -Uri "$fileURL" -OutFile = "$outFile";
+    if ($fileHash.Length -eq 32) { # MD5
+
+    } elseif ($fileHash.Length -eq 40) { # SHA-1
+
+    } elseif ($fileHash.Length -eq 64) { # SHA-256
+
+    } elseif (($fileHash -eq "") -or ($null -eq $fileHash)) { # No hash provided - skip
+
+    } else { #Invalid hash - error.
+        # THROW EXCEPTION
+    }
+    
+}
+# Step-by-step Parts
+#Part 1 - Download Required Files
+#Part 2 - Run Required Files
+#Part 3 - Validate Installation
+#Part 4 - Cleanup
